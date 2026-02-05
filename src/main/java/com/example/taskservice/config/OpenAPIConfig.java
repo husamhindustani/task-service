e1775147fcd1ead05b39d5fadc1dcc9ed32fc4f5
Husam Haque <husam.haque@D11-Husam.Haque>
2026-02-05 22:37:24 +0530

    description of swagger doc updated

--- src/main/java/com/example/taskservice/config/OpenAPIConfig.java
+++ src/main/java/com/example/taskservice/config/OpenAPIConfig.java
@@ -1,7 +1,6 @@
 package com.example.taskservice.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
@@ -30,22 +29,14 @@ public class OpenAPIConfig {
                         .title("Task Service API")
                         .version("1.0.0")
                         .description("""
-                                A simple task management REST API for learning Docker and Kubernetes.
+                                A simple task management REST API
                                 
                                 ## Features
                                 - Create, read, update, and delete tasks
                                 - Filter tasks by status
                                 - Search tasks by title
                                 
-                                ## Learning Project
-                                This API is part of a hands-on course covering:
-                                - Spring Boot REST APIs
-                                - Docker containerization
-                                - Kubernetes orchestration
                                 """)
-                        .contact(new Contact()
-                                .name("Task Service Team")
-                                .email("support@example.com"))
                         .license(new License()
                                 .name("MIT License")
                                 .url("https://opensource.org/licenses/MIT")))
